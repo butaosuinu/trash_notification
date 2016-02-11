34aff53ad402ca493c4e548ece8dce1341518d5b
butaosuinu <buta.osuinu@gmail.com>
2016-02-11 20:36:16 +0900

    add jquery

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var Window = require('browser-window');
 var mainWindow = null;
 
 app.on('ready', function() {
-    mainWindow = new Window({width : 400, height : 220});
+    mainWindow = new Window({width : 400, height : 220, 'node-integration': false});
     mainWindow.loadUrl('file://' + __dirname + '/index.html');
 });
