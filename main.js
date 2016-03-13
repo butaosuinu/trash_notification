89659028214304a7b4b11f0b3f9e464e8d83d4ac
butaosuinu <buta.osuinu@gmail.com>
2016-03-13 16:34:25 +0900

    add menu bar icon

--- main.js
+++ main.js
@@ -6,4 +6,7 @@ var mainWindow = null;
 app.on('ready', function() {
     mainWindow = new Window({width : 450, height : 250, 'node-integration': false});
     mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+    var Tray = require('tray');
+    var appIcon = new Tray(__dirname + '/image/trash_icon.png');
 });
